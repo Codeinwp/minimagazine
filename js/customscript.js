a8fbf5557859f7f26988e00a07ccffecad408ab3
mariusalex20 <cristea.marius.alexandru@gmail.com>
2014-04-18 18:03:08 +0300

    cleaning

--- js/customscript.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function () { 
-	jQuery('ul.sub-menu').parent().css('padding-bottom','0');
-	jQuery('ul.sub-menu').parent().css('border-bottom','none');
-	
-	if(jQuery('#headerOuter').hasClass('tmp')) {
-		jQuery('#headerOuter .postDetails h1').css('color','#5d5d5d');
-	}
-});
