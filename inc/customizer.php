aa4ab7bb09494c987797b0137f161a8611fd0a1f
rodica-andronache <rodik_andronache@yahoo.com>
2014-06-17 15:06:35 +0300

    Updated theme uri, documentation link and forum link

--- inc/customizer.php
+++ inc/customizer.php
@@ -18,7 +18,7 @@ function cwp_customize_register( $wp_customize ) {
 	$wp_customize->add_section( 'codeinwp_theme_notes' , array(
 		'title'      => __('ThemeIsle theme notes','codeinwp'),
 		'description' => sprintf( __( "Thank you for being part of this! We've spent almost 6 months building ThemeIsle without really knowing if anyone will ever use a theme or not, so we're very grateful that you've decided to work with us. Wanna <a href='http://themeisle.com/contact/' target='_blank'>say hi</a>?
-		<br/><br/><a href='http://themeisle.com/demo/?theme=MiniMagazine' target='_blank' />View Theme Demo</a> | <a href='http://themeisle.com/forums/forum/minimagazine/' target='_blank'>Get theme support</a>")),
+		<br/><br/><a href='http://themeisle.com/demo/?theme=MiniMagazine' target='_blank' />View Theme Demo</a> | <a href='http://themeisle.com/forums/forum/minimagazine' target='_blank'>Get theme support</a><br/><br/><a href='http://themeisle.com/documentation-minimagazine' target='_blank'>Documentation</a>")),
 		'priority'   => 30,
 	));
 	$wp_customize->add_setting(
